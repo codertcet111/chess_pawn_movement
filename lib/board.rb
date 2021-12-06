ab0cbec86f9421b994b1cdc024241dd72965bef2
Shubham Mishra <ramsmishra111@gmail.com>
2021-12-06 21:15:13 +0530

    made the input in the iteration

---
+++ lib/board.rb
@@ -0,0 +1,56 @@
+class Board
+  attr_accessor :pawn_x_position, :pawn_y_position, :pawn_color, :pawn_direction, :pawn_icon
+
+  def intialize
+    @pawn_x_position = nil
+    @pawn_y_position = nil
+    @pawn_color = nil
+    @pawn_direction = nil
+    @pawn_icon = nil
+  end
+
+  def self.pawn_icons
+    {'NORTH': '^', 'SOUTH': 'v', 'WEST': '<', 'EAST': '>'}
+  end
+
+  def display
+    clear_terminal
+    print "PAWN SIMULATION: Let's move the pawn \n\n"
+    puts '   ┌────┬────┬────┬────┬────┬────┬────┬────┐'
+    
+    #Set and display all the rows
+    display_rows
+    
+    puts '   └────┴────┴────┴────┴────┴────┴────┴────┘'
+    puts "     1    2    3    4    5    6    7    8  \n\n"
+  end
+
+  def clear_terminal
+    system 'clear'
+    system 'cls'
+  end
+
+  def display_rows
+    (1..8).each do |row_number|
+      display_row(row_number)
+      display_row_seprator unless row_number == 8
+    end
+  end
+
+  def display_row(number)
+    print "#{9 - number}  "
+    # Can check if current Y location is occupied by Pawn or not
+    if @pawn_y_position == number
+      (1..8).each do |col_number|
+        print @pawn_x_position == col_number ?  "│ #{@pawn_icon}  " : '│    '
+      end
+    else
+      print '│    ' * 8
+    end
+    puts '│'
+  end
+
+  def display_row_seprator
+    puts '   ├────┼────┼────┼────┼────┼────┼────┼────┤'
+  end
+end
--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,60 @@
+require_relative "board.rb"
 class Game
+  attr_accessor :board
+
+  def initialize
+    @board = Board.new
+  end
+
+  def play
+    @board.display
+    start
+  end
+
+  def start
+    until false
+      next_input
+      @board.display
+    end
+  end
+
+  def next_input
+    print "Give next command, in the below format:"
+    puts "PLACE X,Y,F,C"
+    puts "MOVE X"
+    puts "LEFT"
+    puts "RIGHT"
+    puts "REPORT\n"
+
+    loop do
+      move = get_move
+      valid_move = true
+      break if valid_move
+      print "\nThat is a Invalid move. Please try again:\n> "
+    end
+
+    move
+  end
+
+  def move
+    # Make actual chages for :pawn_x_position, :pawn_y_position, :pawn_color, :pawn_direction, :pawn_icon of board
+    
+  end
+
+  def get_move
+    move = input
+    until move_formats(move)
+      print "\nThat doesn't appear to be in the correct format. Please see the above instructions.\n> "
+      move = input
+    end
+    move
+  end
+
+  def input
+    gets.chomp
+  end
+
+  def move_formats(input_str)
+    true
+  end
 end
